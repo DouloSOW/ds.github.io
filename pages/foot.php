b4d011eaad25a592f8264867fe9d5838f743fa89
Doulo <doulosow164@gmail.com>
2024-09-22 14:41:02 -0400

    empty

--- pages/foot.php
+++
@@ -1,8 +0,0 @@
-
-</br></br></br></br></br></br>
-<div class="part3">
-</br></br>
-<p class=designed> © Mars 2021,  ECOLE NATIONALE DE LA STATISTIQUE ET DE L'ANALYSE ECONOMIQUE (ENSAE-Dakar) </br></br>
-    Tous droits réservés. Designed By Doulo SOW, ITS4.</p>
- </div>
- 
